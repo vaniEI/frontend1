d93db32e2c6c0ee55698f698936e33edc54b2446
eiofficialgit <137988705+eiofficialgit@users.noreply.github.com>
2023-08-02 17:30:03 +0530

    Update MainController.java

--- src/main/java/com/skyexchange/controller/MainController.java
+++ src/main/java/com/skyexchange/controller/MainController.java
@@ -2,6 +2,7 @@ package com.skyexchange.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 @CrossOrigin("*")
