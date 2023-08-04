49559628b5eb2899b3c14632ec4c4fa4f119c09c
eiofficialgit <137988705+eiofficialgit@users.noreply.github.com>
2023-08-04 18:17:09 +0530

    Update MainController.java

--- src/main/java/com/skyexchange/controller/MainController.java
+++ src/main/java/com/skyexchange/controller/MainController.java
@@ -1,14 +1,13 @@
 package com.skyexchange.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 
-	@GetMapping("/")
+	@RequestMapping("/")
 	public String admin() {
 		return "index";
 	}
