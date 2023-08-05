0cecf18b2d6f477063900cf134f0e31b0f3b8c21
eiofficialgit <137988705+eiofficialgit@users.noreply.github.com>
2023-08-05 11:26:06 +0530

    Update SkyExchangeApplication.java

--- src/main/java/com/skyexchange/SkyExchangeApplication.java
+++ src/main/java/com/skyexchange/SkyExchangeApplication.java
@@ -2,8 +2,10 @@ package com.skyexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages="com.skyexchange.controller")
 public class SkyExchangeApplication {
 
 	public static void main(String[] args) {
