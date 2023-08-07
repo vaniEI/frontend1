1825c14c4c345cd3e3535d92a2c929cec4f53d7c
bhabanieminenceinnovation <bhabani.eminenceinnovation@gmail.com>
2023-08-07 12:28:30 +0530

    thymeleaf first test

--- src/main/java/com/skyexchange/controller/MainController.java
+++ src/main/java/com/skyexchange/controller/MainController.java
@@ -1,18 +1,21 @@
 package com.skyexchange.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
 	@RequestMapping("/")
-	public String admin() {
+	public String admin(Model model) {
+		model.addAttribute("title", "Login - Sky Exchange");
 		return "index";
 	}
 	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("title", "Home - Sky Exchange");
 		return "home";
 	}
 
